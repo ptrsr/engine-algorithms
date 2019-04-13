b48428a2c5f4b59c073427312fea5aa018bc5f2b
Ruud Peters <ruud_peters96@hotmail.com>
2019-04-13 22:31:02 +0200

    updated gitignore

---
+++ test/core/object.test.cpp
@@ -0,0 +1,94 @@
+#include <gtest/gtest.h>
+#include <core/object.hpp>
+#include <iostream>
+#include <exception>
+#include <vec.aux.hpp>
+
+TEST(Object, Init) {
+    // assert init with no parameters means unit mat4
+    Object object = Object();
+    ASSERT_EQ(object.transform, glm::mat4(1));
+
+    // assert init with mat
+    object = Object(glm::mat4(2));
+    ASSERT_EQ(object.transform, glm::mat4(2));
+}
+
+TEST(Object, Position) {
+    Object_ptr object = std::make_shared<Object>();
+    ASSERT_EQ(object->GetPosition(), glm::vec3());
+
+    // assert if position is set
+    object->SetPosition(glm::vec3(1, 2, 3));
+    ASSERT_EQ(object->GetPosition(), glm::vec3(1, 2, 3));
+
+    // assert if right column is set
+    ASSERT_EQ(object->GetTransform()[3], glm::vec4(object->GetPosition(), 1));
+}
+
+TEST(Object, rotation) {
+    Object_ptr object = std::make_shared<Object>();
+    object->Rotate(glm::pi<float>(), glm::vec3(0, 1, 0));
+
+
+    glm::mat2 test(1,2,3,4);
+    std::cout << test << std::endl;
+    EXPECT_TRUE(CompareVecsAbs(glm::mat<2, 2, float>(1.0003f,1.f,1.f,1.f), glm::mat<2, 2, float>(3.f,1.f,1.000006f,6.f), 1.f));
+    // std::cout << object->GetTransform() << std::endl;
+    // std::cout << glm::mat4(-1, 0, 0, 0,
+    //                0, 1, 0, 0,
+    //                0, 0,-1, 0,
+    //                0, 0, 0, 1) << std::endl;
+
+    // glm::mat4
+
+    // ASSERT_NEAR(object->GetTransform(), 
+    //     glm::mat4(-1, 0, 0, 0,
+    //                0, 1, 0, 0,
+    //                0, 0,-1, 0,
+    //                0, 0, 0, 1), 0.000001f
+    // );
+
+}
+
+TEST(Object, Parent) {
+    // throw when trying to unparent when object has no parent
+    Object_ptr parent = std::make_shared<Object>();
+    ASSERT_THROW(parent->UnParent(), std::runtime_error);
+
+    // assert no children after init
+    auto& children = parent->GetChildren();
+    ASSERT_EQ(children.size(), 0);
+
+    // assert adding child results in correct parent
+    Object_ptr child = std::make_shared<Object>();
+    parent->AddChild(child);
+    ASSERT_EQ(child->GetParent(), parent);
+
+    // assert chilren size 1
+    // (GetChildren returned a const ref)
+    ASSERT_EQ(children.size(), 1);
+
+    // assert child in children list is indeed same child
+    ASSERT_EQ(children[0], child);
+    ASSERT_EQ(children[0]->GetParent(), parent);
+
+    // assert if unparenting removes relation in both child and parent
+    child->UnParent();
+    ASSERT_EQ(children.size(), 0);
+    ASSERT_EQ(child->GetParent(), nullptr);
+}
+
+TEST(Object, Remove) {
+    Object_ptr parent = std::make_shared<Object>();
+    {
+        Object_ptr child = std::make_shared<Object>();
+        parent->AddChild(child);
+
+        // assert if two shared pointers exist
+        ASSERT_EQ(parent->GetChildren()[0].use_count(), 2);
+    }
+    // assert after closing scope if pointer count drops to one
+    ASSERT_EQ(parent->GetChildren()[0].use_count(), 1);
+}
+
---
+++ test/vec.aux.hpp
@@ -0,0 +1,88 @@
+#include <glm.hpp>
+#include <gtest/gtest.h>
+
+#include <string>
+#include <list>
+#include <vector>
+
+#define CONSOLE_RESET  "\033[0m"
+#define CONSOLE_RED    "\033[1;31m"
+#define CONSOLE_YELLOW "\033[1;33m"
+
+#define MAX_LOG_PRECISION 20
+
+typedef unsigned int uint;
+typedef glm::vec<2, unsigned int> vec2i;
+
+template <int cols, int rows, typename type>
+std::string MatDifOut(glm::mat<cols, rows, type> mat, 
+                      glm::mat<cols, rows, bool> faulty,
+                      std::string consoleColor) 
+{
+	std::stringstream ts, ss;
+
+	// for adding extra spacing for easier reading later on
+	glm::vec<cols, uint> maxWidthPerCol = glm::vec<cols, uint>(0);
+	glm::mat<cols, rows, uint> widthPerEntry = glm::mat<cols, rows, uint>(0);
+
+	for (uint j = 0; j < cols; ++j) {
+		for (uint i = 0; i < rows; ++i) {
+			// put entry in temporary stream
+			ts << mat[j][i];
+			// count length
+			uint count = ts.str().size();
+			if (count > maxWidthPerCol[j]) {
+				// maximum width of each column
+				maxWidthPerCol[j] = count;
+			}
+			// set entry width
+			widthPerEntry[j][i] = count; 
+
+			// reset temporary stream
+			ts.str("");
+		}
+	}
+	for (uint i = 0; i < rows; ++i) {
+		ss << "\n|";
+		for (uint j = 0; j < cols; ++j) {
+			if (j != 0) {
+				ss << ", ";
+			}
+			if (faulty[j][i]) {
+				ss << consoleColor << mat[j][i] << CONSOLE_RESET;
+			} else {
+				ss << mat[j][i];
+			}
+			// add extra spacing
+			for (uint i = 0; i < maxWidthPerCol[j] - widthPerEntry[j][i]; ++i) {
+				ss << " ";
+			}
+		}
+		ss << "|";
+	}
+	ss << "\n";
+	return ss.str();
+}
+
+template <int cols, int rows, typename type>
+::testing::AssertionResult CompareVecsAbs(glm::mat<cols, rows, type> expected,
+                                          glm::mat<cols, rows, type> result,
+                                          type maxDelta) 
+{
+	bool error = false;
+	glm::mat<cols, rows, bool> faulty(false);
+	for (uint j = 0; j < cols; ++j) {
+		for (uint i = 0; i < rows; ++i) {
+			if (std::abs(expected[j][i] - result[j][i]) > maxDelta) {
+				faulty[j][i] = true;
+				error = true;
+			}
+		}
+	}
+	if (error) {
+		return ::testing::AssertionFailure() << "\nExpected:"
+			       << MatDifOut(expected, faulty, CONSOLE_YELLOW) << "\nResult:"
+			       << MatDifOut(result, faulty, CONSOLE_RED);
+	}
+	return ::testing::AssertionSuccess();
+}
