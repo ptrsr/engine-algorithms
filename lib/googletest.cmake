d51d95ca4d8700998534170b6f376fa19d4fdb4f
Ruud Peters <ruud_peters96@hotmail.com>
2019-05-05 17:41:52 +0200

    fixed lib config

--- lib/googletest.cmake
+++ lib/googletest.cmake
@@ -1,6 +1,8 @@
 cmake_minimum_required(VERSION 2.8.2)
 include(ExternalProject)
 
+set(CMAKE_INSTALL_LIBDIR lib)
+
 ExternalProject_Add(googletest PREFIX googletest
   GIT_REPOSITORY    https://github.com/google/googletest.git
   GIT_TAG           release-1.8.1
@@ -11,7 +13,8 @@ ExternalProject_Add(googletest PREFIX googletest
     "-DINSTALL_GTEST=ON"
     "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
     "-DCMAKE_BUILD_TYPE=$<$<CONFIG:Debug>:Debug>$<$<CONFIG:Release>:Release>"
-
+    "-DCMAKE_INSTALL_LIBDIR=lib" # https://cmake.org/cmake/help/v3.14/module/GNUInstallDirs.html
+                                 # force libdir to be 'lib'. Otherwise it'll default to system arch (e.g. lib64)
   CMAKE_CACHE_ARGS
     "-DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}"
     "-DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}"
